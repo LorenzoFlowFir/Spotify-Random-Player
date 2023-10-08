b2995ad625aa6ce56e2648a007d6656121e65563
LorenzoFlowFir <46107911+LorenzoFlowFir@users.noreply.github.com>
2023-10-08 19:30:13 +0200

    Ajout du song challenge réussi

--- src/app/pages/song-challenge/song-challenge.component.ts
+++ src/app/pages/song-challenge/song-challenge.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { SongChallengeService } from 'src/app/services/SongChallenge.service';
+import { DiscordDataService } from '../../services/discord-data.service';
 
 @Component({
   selector: 'app-song-challenge',
   templateUrl: './song-challenge.component.html',
-  styleUrls: ['./song-challenge.component.scss']
+  styleUrls: ['./song-challenge.component.scss'],
 })
 export class SongChallengeComponent {
   public loader = this.songChallengeService.loader;
+  public covers = [];
 
-  constructor(private songChallengeService:SongChallengeService){}
-  
+  constructor(
+    private songChallengeService: SongChallengeService,
+    private discordDataService: DiscordDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.discordDataService.getDiscordData().subscribe(
+      (data) => {
+        this.covers = data;
+        console.log(data);
+        this.songChallengeService.songChallenge(this.covers);
+      },
+      (error) => console.error(error)
+    );
+  }
 }
