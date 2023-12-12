43cd4f9290fbfda6732da2d1ec6cbe4f53750789
LorenzoFlowFir <46107911+LorenzoFlowFir@users.noreply.github.com>
2023-12-12 05:40:03 +0100

    change

--- src/app/services/SongChallenge.service.ts
+++ src/app/services/SongChallenge.service.ts
@@ -86,114 +86,121 @@ export class SongChallengeService {
           }
         }
       };
+    } else {
+      shareBtn.textContent = 'Partager la playlist';
+      shareBtn.onclick = function () {
+        if (navigator.share) {
+          navigator
+            .share({
+              title: 'Voici une superbe playlist Spotify!',
+              text: `Jetez un coup d'œil à cette playlist: "${titre[0]}"`,
+              url: cover[0],
+            })
+            .then(() => console.log('Partage réussi'))
+            .catch((error) => console.log('Erreur de partage', error));
+        } else {
+          console.log("Votre navigateur ne supporte pas l'API de partage Web");
+        }
+      };
     }
 
     playlistDiv?.appendChild(shareBtn);
-    /*
-    fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        
-        //const sendInput = document.createElement("input");
-        //sendInput.type = "text";
-        //sendInput.id = "sendInput";
-        //sendInput.placeholder = "Entrez le lien de votre musique";
-        //playlistDiv?.appendChild(sendInput);
 
-        const moreBtn = document.createElement('button');
-        moreBtn.classList.add('btn');
-        moreBtn.classList.add('btn-primary');
-        moreBtn.id = 'moreBtn';
-        moreBtn.textContent = 'Plus de challenge';
-        let first = false;
-        moreBtn.onclick = () => {
-          if (moreBtn.textContent == 'Plus de challenge' && !first) {
-            moreBtn.textContent = 'Retour';
-            const divmore = document.createElement('div');
-            divmore.style.display = 'flex';
-            divmore.style.flexDirection = 'row';
-            divmore.style.justifyContent = 'space-between';
-            divmore.style.alignItems = 'center';
-            divmore.style.flexWrap = 'wrap';
+    const moreBtn = document.createElement('button');
+    moreBtn.classList.add('btn');
+    moreBtn.classList.add('btn-primary');
+    moreBtn.id = 'moreBtn';
+    moreBtn.textContent = 'Plus de challenge';
+    let first = false;
 
-            const overlay = document.createElement('div'); // Création d'un div en overlay
-            overlay.style.position = 'fixed';
+    //const sendInput = document.createElement("input");
+    //sendInput.type = "text";
+    //sendInput.id = "sendInput";
+    //sendInput.placeholder = "Entrez le lien de votre musique";
+    //playlistDiv?.appendChild(sendInput);
+    //TODO : Créer dirrectement cette partie dans un composant
+    moreBtn.onclick = () => {
+      if (moreBtn.textContent == 'Plus de challenge' && !first) {
+        moreBtn.textContent = 'Retour';
+        const divmore = document.createElement('div');
+        divmore.style.display = 'flex';
+        divmore.style.flexDirection = 'row';
+        divmore.style.justifyContent = 'space-between';
+        divmore.style.alignItems = 'center';
+        divmore.style.flexWrap = 'wrap';
+
+        const overlay = document.createElement('div'); // Création d'un div en overlay
+        overlay.style.position = 'fixed';
+        overlay.style.display = 'none';
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.top = '0';
+        overlay.style.left = '0';
+        overlay.style.right = '0';
+        overlay.style.bottom = '0';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        overlay.style.zIndex = '2';
+        overlay.style.cursor = 'pointer';
+
+        const largeImage = document.createElement('img'); // Création de l'élément d'image en grand
+        largeImage.style.position = 'absolute';
+        largeImage.style.top = '50%';
+        largeImage.style.left = '50%';
+        largeImage.style.transform = 'translate(-50%,-50%)';
+        largeImage.style.maxHeight = '80%';
+        largeImage.style.maxWidth = '80%';
+        largeImage.style.zIndex = '3';
+
+        document.body.appendChild(overlay);
+        overlay.appendChild(largeImage);
+
+        for (let i = 1; i < 10; i++) {
+          const p = document.createElement('p');
+          const img = document.createElement('img');
+          const innerDiv = document.createElement('div');
+          img.src = cover[i];
+          img.alt = `Cover de ${titre[i]}`;
+          img.style.width = '100px';
+          innerDiv.appendChild(img);
+          innerDiv.appendChild(p);
+          innerDiv.style.display = 'flex';
+          innerDiv.style.flexDirection = 'column';
+          innerDiv.style.justifyContent = 'center';
+          innerDiv.style.alignItems = 'center';
+          p.appendChild(
+            document.createTextNode(
+              this.getCreationDateFromTitle(Number(day[i]))
+            )
+          );
+          divmore.appendChild(innerDiv);
+
+          img.addEventListener('click', () => {
+            // Ajout d'un gestionnaire d'événements click sur chaque image
+            largeImage.src = img.src; // Changer le src de l'image en grand pour correspondre à l'image cliquée
+            overlay.style.display = 'block'; // Afficher l'overlay
+          });
+        }
+
+        document.getElementById('moreChallenge')?.appendChild(divmore);
+        document.getElementById('moreChallenge')!.style.display = 'block';
+
+        // Fermer l'overlay quand on clique en dehors de l'image
+        overlay.addEventListener('click', (event) => {
+          if (event.target == overlay) {
             overlay.style.display = 'none';
-            overlay.style.width = '100%';
-            overlay.style.height = '100%';
-            overlay.style.top = '0';
-            overlay.style.left = '0';
-            overlay.style.right = '0';
-            overlay.style.bottom = '0';
-            overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-            overlay.style.zIndex = '2';
-            overlay.style.cursor = 'pointer';
-
-            const largeImage = document.createElement('img'); // Création de l'élément d'image en grand
-            largeImage.style.position = 'absolute';
-            largeImage.style.top = '50%';
-            largeImage.style.left = '50%';
-            largeImage.style.transform = 'translate(-50%,-50%)';
-            largeImage.style.maxHeight = '80%';
-            largeImage.style.maxWidth = '80%';
-            largeImage.style.zIndex = '3';
-
-            document.body.appendChild(overlay);
-            overlay.appendChild(largeImage);
-
-            for (let i = 1; i < 10; i++) {
-              const p = document.createElement('p');
-              const img = document.createElement('img');
-              const innerDiv = document.createElement('div');
-              img.src = latestPlaylist[i].images[0].url;
-              img.alt = `Cover de ${latestPlaylist[i].name}`;
-              img.style.width = '100px';
-              innerDiv.appendChild(img);
-              innerDiv.appendChild(p);
-              innerDiv.style.display = 'flex';
-              innerDiv.style.flexDirection = 'column';
-              innerDiv.style.justifyContent = 'center';
-              innerDiv.style.alignItems = 'center';
-              p.appendChild(
-                document.createTextNode(
-                  this.getCreationDateFromTitle(latestPlaylist[i].name)
-                )
-              );
-              divmore.appendChild(innerDiv);
-
-              img.addEventListener('click', () => {
-                // Ajout d'un gestionnaire d'événements click sur chaque image
-                largeImage.src = img.src; // Changer le src de l'image en grand pour correspondre à l'image cliquée
-                overlay.style.display = 'block'; // Afficher l'overlay
-              });
-            }
-
-            document.getElementById('moreChallenge')?.appendChild(divmore);
-            document.getElementById('moreChallenge')!.style.display = 'block';
-
-            // Fermer l'overlay quand on clique en dehors de l'image
-            overlay.addEventListener('click', (event) => {
-              if (event.target == overlay) {
-                overlay.style.display = 'none';
-              }
-            });
-            first = true;
-          } else if (moreBtn.textContent == 'Retour') {
-            document.getElementById('moreChallenge')!.style.display = 'none';
-            moreBtn.textContent = 'Plus de challenge';
-          } else if (moreBtn.textContent == 'Plus de challenge') {
-            moreBtn.textContent = 'Retour';
-            document.getElementById('moreChallenge')!.style.display = 'block';
           }
-        };
-        playlistDiv?.appendChild(moreBtn);
+        });
+        first = true;
+      } else if (moreBtn.textContent == 'Retour') {
+        document.getElementById('moreChallenge')!.style.display = 'none';
+        moreBtn.textContent = 'Plus de challenge';
+      } else if (moreBtn.textContent == 'Plus de challenge') {
+        moreBtn.textContent = 'Retour';
+        document.getElementById('moreChallenge')!.style.display = 'block';
+      }
+    };
+    playlistDiv?.appendChild(moreBtn);
 
-        document.getElementById('loadersongchallenge')!.style.display = 'none';
-      })
-      .catch((error) => console.error('Erreur :', error));*/
+    document.getElementById('loadersongchallenge')!.style.display = 'none';
   }
 }
